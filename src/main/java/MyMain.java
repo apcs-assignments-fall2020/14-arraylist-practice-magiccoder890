8bd96c45770b79d32badc4a84b2c970f59f9721b
magiccoder890 <23fzhu@athenian.org>
2020-11-09 12:09:11 +0000

    This is my homework for weekend

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,38 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
+        int oddnumber =  0;
         // REPLACE WITH YOUR CODE
-        return 0;
+        for (int i : list){
+            if (i % 2 == 1 || i % 2 == -1){
+                oddnumber += 1;
+            }
+        }
+        return oddnumber;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
+        int list = 0;
         // REPLACE WITH YOUR CODE
-        return false;
+            for(int i : list1) {
+                for (int u : list2) {
+                    if (i == u) {
+                        list += 1;
+                    }
+                }
+            }
+        return list>0;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
         // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> newlist = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++){
+            newlist.add(arr[i]);
+        }
+        return newlist;
     }
 
 
